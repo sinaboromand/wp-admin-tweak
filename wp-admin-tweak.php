74f959837c819865d8f639650a43f0b1d16a5a8f
SenC <sinaboromand@gmail.com>
2021-05-27 20:51:43 +0430

    adding iranyekan fonts and applying some styles

--- wp-admin-tweak.php
+++ wp-admin-tweak.php
@@ -8,16 +8,30 @@
  * Text Domain: sb-wpat
  */
 
-if (!defined('ABSPATH')) { exit; }/**
+if (!defined('ABSPATH')) { exit; }
 
+/**
  *
- * * Plugin Defines
+ * Plugin basic defines
  *
  * @return void
- * @author @sinaboromand
- * @since  1.0.0
+ * @version 1.0
+ * @since 1.0
  */
 define("WPAT_DIR",    plugin_dir_path(__FILE__));
 define("WPAT_URI",    plugin_dir_url(__FILE__));
-define("WPAT_INCS",   SINDEV_DIR.'includes/');
-define("WPAT_ASSETS", SINDEV_URI.'assets/');
+define("WPAT_ASSETS", WPAT_URI.'assets/');
+
+/**
+ *
+ * Plugin basic defines
+ *
+ * @return void
+ * @version 1.0
+ * @since 1.0
+ */
+
+add_action('admin_enqueue_scripts','wpat_assets');
+function wpat_assets() {
+    wp_enqueue_style('wpat-styles',WPAT_ASSETS.'/css/style.css');
+}
