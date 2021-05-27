6d91c27d70f65442d3c41969243688f6b8cbe74b
SenC <sinaboromand@gmail.com>
2021-05-27 18:44:23 +0430

    adding plugin defines

---
+++ wp-admin-tweak.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Plugin Name: بهینه ساز ظاهر وردپرس
+ * Description: این افزونه فونت های پنل ادمین را به ایران یکان تغییر میدهد و باعث خوانایی بیشترتمام بخش ها خواهدشد.
+ * Version: 1.0
+ * Author:سینابرومند
+ * Author URI: http://sinaboromand.ir
+ * Text Domain: sb-wpat
+ */
+
+if (!defined('ABSPATH')) { exit; }/**
+
+ *
+ * * Plugin Defines
+ *
+ * @return void
+ * @author @sinaboromand
+ * @since  1.0.0
+ */
+define("WPAT_DIR",    plugin_dir_path(__FILE__));
+define("WPAT_URI",    plugin_dir_url(__FILE__));
+define("WPAT_INCS",   SINDEV_DIR.'includes/');
+define("WPAT_ASSETS", SINDEV_URI.'assets/');
